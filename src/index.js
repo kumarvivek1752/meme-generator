5fbc95caef0b2b994b1f6016c096bc95bbfdda1c
Vivek Kumar <Kumarvivek1752@gmail.com>
2023-05-25 09:39:58 +0530

    added form

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
+import Meme from './components/Meme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Meme />
   </React.StrictMode>
 );
 
